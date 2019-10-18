446ac502ea37748421388b937b4ad0d2240647d9
Caroline Cran <cranch.caroline@gmail.com>
2019-10-17 20:24:10 -0400

    user navbar fontawesome icons and font revamp

--- client/src/components/SideNavPage/index.js
+++ client/src/components/SideNavPage/index.js
@@ -7,31 +7,35 @@ export default props => {
     <Menu width={300}>
 
       <a className="menu-item" href="/user">
+      <i class="fa fa-home"></i>&nbsp;
         Home
       </a>
 
       <a className="menu-item" onClick={props.searchOption}>
-      <i class="fas fa-search"></i>
+      <i class="fa fa-search"></i>&nbsp;
         Search
       </a>
 
       <a className="menu-item" onClick={props.createOption}>
+      <i class="fa fa-plus-circle"></i>&nbsp;
         Create
       </a>
 
       <a className="menu-item" onClick={props.editOption}>
+      <i class="fa fa-edit"></i>&nbsp;
         View/Edit
       </a>
 
       <a className="menu-item" onClick={props.requestOption}>
+      <i class="fa fa-camera-retro"></i>&nbsp;
         Request
       </a>
 
       <a className="menu-item" onClick={props.viewrequestsOption}>
-        View Requests
+      <i class="fa fa-hashtag"></i>&nbsp;
+        View My Caps
       </a>
     </Menu>
   );
 };
 
-// export default SideNavPage;
