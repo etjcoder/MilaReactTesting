e9027edc02efbaf09aae2742c57caec11207d24e
Evan Cleary <evanjcleary@gmail.com>
2019-10-28 19:23:44 -0400

    update sidenav for caroline

--- client/src/components/SideNavPage/index.js
+++ client/src/components/SideNavPage/index.js
@@ -7,76 +7,66 @@ export default props => {
     <Menu width={285}>
 
       <a className="menu-item" href="/user">
-      <i class="fa fa-home"></i>&nbsp;
-        Home
+        <i class="fa fa-home"></i>&nbsp;
+          Home
       </a>
-
-      <a className="menu-item" onClick={props.editProfile}>
-      <i class="fa fa-user-circle"></i>&nbsp;
-        Edit Profile
-      </a>   
 
       <a className="menu-item" onClick={props.searchOption}>
-      <i class="fa fa-search"></i>&nbsp;
-        Search
-      </a>
-
-      <a className="menu-item" onClick={props.createOption}>
-      <i class="fa fa-plus-circle"></i>&nbsp;
-        Create
-      </a>
-
-      <a className="menu-item" onClick={props.editOption}>
-      <i class="fa fa-edit"></i>&nbsp;
-        View/Edit
+        <i class="fa fa-search"></i>&nbsp;
+          Search
       </a>
 
       <a className="menu-item" onClick={props.requestOption}>
-      <i class="fa fa-camera-retro"></i>&nbsp;
-        Request a Caption
+        <i class="fa fa-camera-retro"></i>&nbsp;
+          Request a Caption
       </a>
 
       <a className="menu-item" onClick={props.viewMyRequests}>
-      <i class="fa fa-hashtag"></i>&nbsp;
-        View My Requests
+        <i class="fa fa-hashtag"></i>&nbsp;
+          View My Requests
       </a>
 
       <a className="menu-item" onClick={props.viewrequestsOption}>
-      <i class="fa fa-users"></i>&nbsp;
-        Community Requests
+        <i class="fa fa-users"></i>&nbsp;
+          Community Requests
+      </a>
+
+      <a className="menu-item" onClick={props.editProfile}>
+        <i class="fa fa-user-circle"></i>&nbsp;
+          Edit Profile
       </a>
 
       <a className="menu-item" onClick={props.logOut}>
-      <i class="fa fa-sign-out"></i>&nbsp;
-        Logout
-      </a>  
+        <i class="fa fa-sign-out"></i>&nbsp;
+          Logout
+      </a>
 
 
       <a className="admin-item">
-        MILA Admin 
-      </a> 
+        MILA Admin
+      </a>
 
       {/* insert admin options here when admin page becomes obsolete */}
-      
+
 
       <a className="menu-item" onClick={props.createAdminOption}>
-      <i class="fa fa-plus-circle"></i>&nbsp;
-        Create Mila Caption
+        <i class="fa fa-plus-circle"></i>&nbsp;
+          Create Mila Caption
       </a>
 
       <a className="menu-item" onClick={props.editMilaOption}>
-      <i class="fa fa-edit"></i>&nbsp;
-        View/Edit Captions
+        <i class="fa fa-edit"></i>&nbsp;
+          View/Edit Captions
       </a>
 
       <a className="menu-item" onClick={props.createCategoryOption}>
-      <i class="fas fa-folder-plus"></i>&nbsp;
-        Create Category
+        <i class="fas fa-folder-plus"></i>&nbsp;
+          Create Category
       </a>
 
       <a className="menu-item" onClick={props.featureOption}>
-      <i class="fa fa-star"></i>&nbsp;
-        Feature Captions
+        <i class="fa fa-star"></i>&nbsp;
+          Feature Captions
       </a>
 
     </Menu>
